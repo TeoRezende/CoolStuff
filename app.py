8cdbcda011b8a114ea7b8b16187afd6e83f997f0
TeoRezende <18126@wc.school.nz>
2022-03-03 15:15:49 +1300

    Smile Cafe

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
-
+import sqlite3
 app = Flask(__name__)
+DATABASE = "C:/Users/d.banseman/OneDrive - Wellingaton College/13DTS/Python2022/Smile/Smile.db"
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 
 @app.route('/')

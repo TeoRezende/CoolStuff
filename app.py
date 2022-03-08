f7f99645dc502f2239c3115dd6a2b2dd0ab6dae0
TeoRezende <18126@wc.school.nz>
2022-03-09 12:29:02 +1300

    Smile Cafe

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import sqlite3
 app = Flask(__name__)
 DATABASE = "C:/Users/d.benseman/OneDrive - Wellington College/13DTS/Python2022/Smile/Smile.db"
-
+#test
 def create_connection(db_file):
     try:
         connection = sqlite3.connect(db_file)
@@ -10,6 +10,8 @@ def create_connection(db_file):
     except Error as e:
         print(e)
     return None
+
+
 
 
 @app.route('/')
@@ -20,7 +22,17 @@ def render_homepage():
 @app.route('/menu')
 def render_menu_page():
     return render_template('menu.html')
+    #connect to database
+    con = create_connection(DB_NAME)
+    #fetch datas from the goddamn database
+    query = "SELECT name, description, volume, price, image FROM product"
+    cur = con.cursor() #this does something
 
+    cur = con.execute() #this executed the query
+    product_list = cur.fetchall() #makes the list usable by python
+    con.close()
+
+    return render_template('menu.html', products = product_list)
 
 @app.route('/contact')
 def render_contact_page():

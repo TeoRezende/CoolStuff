22fd7bc3e670707a05539b9ff6a15a6e5bb7e752
TeoRezende <18126@wc.school.nz>
2022-03-18 11:13:07 +1300

    Smile Cafe finished login and signup!

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
+from flask_bcrypt import Bcrypt
+from datetime import datetime
 app = Flask(__name__)
 DATABASE = "C:/Users/18126/Documents/Smile/smile.db"
 #test
@@ -19,6 +21,20 @@ def create_connection(db_file):
 def render_homepage():
     return render_template('home.html')
 
+@app.route('/addtocart/<productid>')
+def addtocart(productid):
+    userid = session['userid']
+    timestamp = datetime.now()
+    print("User {} would like to add {} to cart at {}".format(userid, productid, timestamp))
+
+    query = 'INSERT INTO cart(id, userid, productid, timestamp) VALUES(NULL,?,?,?,?)'
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    cur.execute(query, (userid, productid, timestamp))
+    con.commit()
+    con.close()
+    return redirect(request.referrer)
+
 #menu
 @app.route('/menu')
 def render_menu_page():
@@ -26,7 +42,7 @@ def render_menu_page():
     #connect to database
     con = create_connection(DATABASE)
     #fetch datas from the goddamn database
-    query = "SELECT name, description, volume, price, image FROM product"
+    query = "SELECT name, description, volume, price, image, id FROM product"
     cur = con.cursor() #this does something I don't know
     print("connected")
     cur.execute(query) #this executed the query
@@ -50,9 +66,9 @@ def render_signup_page():
     pword = request.form.get("pword")
     pword2 = request.form.get("pword2")
 
-    con = create_connection(DB_NAME)
+    con = create_connection(DATABASE)
 
-    query = "INSERT INTO customer(id, fname,. lname, email, pword) VALUES(NULL,?,?,?,?)"
+    query = "INSERT INTO customer(id, fname, lname, email, pword) VALUES(NULL,?,?,?,?)"
 
     cur = con.cursor()
     cur.execute(query, (fname, lname, email, pword))
